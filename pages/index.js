e24e3082768f60b6f2bae6a998329a020a991efe
Marc-Antoine Ferland <maferland@hey.com>
2021-01-21 16:51:13 -0500

    fix: typo

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="min-h-screen bg-red-100 text-red-800 font-bold flex items-center justify-center">
         <div>
           <h1 className="text-6xl">Timepaste</h1>
-          <h2 className="text-2xl ml-2">Comming soon!</h2>
+          <h2 className="text-2xl ml-2">Coming soon!</h2>
         </div>
       </div>
     </>
